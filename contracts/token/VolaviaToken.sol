d1bf0ddaf70248c54a90c2185b1c726924b373b6
cheshta <cheshta.sikka@seedcloud.in>
2018-07-23 15:59:03 +0530

    token name restored as first commit

--- contracts/token/VolaviaToken.sol
+++ contracts/token/VolaviaToken.sol
@@ -5,8 +5,8 @@ import "zeppelin-solidity/contracts/math/SafeMath.sol";
 
 contract VolaviaToken is MintableToken {
     using SafeMath for uint256;
-    string public constant name = "Dummy";
-    string public constant symbol = "D";
+    string public constant name = "Volavia";
+    string public constant symbol = "VOL";
     uint8 public constant decimals = 18;
 }
    
